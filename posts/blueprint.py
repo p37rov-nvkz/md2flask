d72e4d3047a1402e0a58a00eac9d9a4381f53cac
Petrov Vladimir <p37rov.nvkz@gmail.com>
2021-02-11 19:58:15 +0700

    Рендеринг  .md -> html

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,8 +1,14 @@
+import markdown
 from flask import Blueprint
+import markdown.extensions.fenced_code
 
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 @posts.route('/')
 def index():
-    return "Blueprint Posts"
+    test_file = posts.open_resource("posts/test.md","r")
+    md_template_string = markdown.markdown(
+        test_file.read(), extension=["fenced_code"]
+    )
+    return md_template_string
