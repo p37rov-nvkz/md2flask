5a76aa7aa9682b09de295f170aa2e7399d5b1e12
Petrov Vladimir <p37rov.nvkz@gmail.com>
2021-02-12 18:03:30 +0700

    Инициализировал misaka экземпляром posts

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,7 +1,11 @@
 from flask import Blueprint
+from flask import render_template
+from flask_misaka import markdown
+from flask_misaka import Misaka
 
 
 posts = Blueprint('posts', __name__, template_folder='templates')
+Misaka(posts)
 
 @posts.route('/')
 def index():
